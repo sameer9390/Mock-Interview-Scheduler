22890ed7c85ffc983595ac2300849172c7876b5e
Seetharam <sadalaseetharam@gmail.com>
2025-06-16 11:39:48 +0530

    Initial commit

---
+++ src/main/java/com/sameer/mockinterview/MockinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.sameer.mockinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockinterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sameer/mockinterview/MockinterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.mockinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockinterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
